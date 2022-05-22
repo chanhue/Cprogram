acb7453bb2ab4fad11c872404953d5e748454610
chanhue <69076478+chanhue@users.noreply.github.com>
2022-05-22 13:46:57 +0000

    5_1_1문제풀이(임시)

--- 연습문제 풀이/ch5/ch5_1_1.c
+++ 연습문제 풀이/ch5/ch5_1_1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int ch5_1_1(void){
-
-  
-  return 0;
+int ch5_1_1(void) {
+	int x1, y1, x2, y2, sq;
+	printf("두 점의 좌표를 입력하시오:");
+	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+	sq = (x2 - x1) * (y2 - y1);
+	printf("직사각형의 넓이는: %d", sq);
+	return 0;
 }
