402b58c7eb98ec40bbc910c6baa72caabd7add72
chanhue <69076478+chanhue@users.noreply.github.com>
2022-05-25 14:45:05 +0000

    ch7_1_3 오류 해결

--- 연습문제 풀이/ch7/ch7_1_3.c
+++ 연습문제 풀이/ch7/ch7_1_3.c
@@ -4,9 +4,9 @@ int ch7_1_3(void) {
 	int num = 1, sum = 0;
 	while (num != 0) {
 		printf("정수를 하나 입력하시오: ");
-		scanf("%d\n", &num);
+		scanf("%d", &num); // scanf("%d\n", &num);에서 개행문자\n 삭제
 		sum += num;
 	}
 	printf("모든 정수의 합: %d", sum);
 	return 0;
-}
+} //개행문자로 인한 출력의 오류?? >> 공부하기
