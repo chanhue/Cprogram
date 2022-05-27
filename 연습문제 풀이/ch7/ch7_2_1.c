0cb8fd720b958abd7d06011b5f7d728a823e2484
chanhue <69076478+chanhue@users.noreply.github.com>
2022-05-27 13:16:10 +0000

    ch7_2_1 풀이중...

--- 연습문제 풀이/ch7/ch7_2_1.c
+++ 연습문제 풀이/ch7/ch7_2_1.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int ch7_2_1(void){
-  
+  int num, sum, i;
+  while(i<5){
+    printf("정수를 입력하시오");
+    scanf("%d",&num);
+    i++;
+  }
+  printf("입력한 수의 합은: %d", sum);
+  return 0;
 }
