dc86c19a53dc2a309b8f2fa246d20848b3434038
chanhue <69076478+chanhue@users.noreply.github.com>
2022-05-28 08:00:10 +0000

    ch7_2_1 풀이 완료(while문의 중첩)

--- 연습문제 풀이/ch7/ch7_2_1.c
+++ 연습문제 풀이/ch7/ch7_2_1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int ch7_2_1(void){
-  int num, sum, i;
+  int num, sum, i=0;
   while(i<5){
-    printf("정수를 입력하시오");
+    printf("1이상의 정수를 입력하시오");
     scanf("%d",&num);
+    while(num<1){
+      printf("입력한 정수는 1이하입니다\n다시 입력하시오");
+      scanf("%d", &num);
+    }
+    sum += num;
     i++;
   }
   printf("입력한 수의 합은: %d", sum);
