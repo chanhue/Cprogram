235b9dcb6c71686abfe7a1315a43dc7127d1ae5e
chanhue <69076478+chanhue@users.noreply.github.com>
2022-06-05 13:22:06 +0000

    ch9_1_1 풀이완료(함수의 활용)

--- 연습문제 풀이/Part 1/ch9/ch9_1_1.c
+++ 연습문제 풀이/Part 1/ch9/ch9_1_1.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
-int compare(int num1, int num2, int num3)
+int compareb(int num1, int num2, int num3);
+int compares(int num1, int num2, int num3);
 
-int ch9_1_1(void){
-  int num1, num2,num3;
-  printf("세 정수를 입력하시오");
-  scanf("%d%d%d",num1, num2,num3);
-  
+int ch9_1_1(void) {
+	int num1, num2, num3;
+	printf("세 정수를 입력하시오");
+	scanf("%d%d%d", &num1, &num2, &num3);
+	printf("가장 큰 수는: %d\n", compareb(num1, num2, num3));
+	printf("가장 작은 수는: %d\n", compares(num1, num2, num3));
+}
+
+int compareb(int num1, int num2, int num3) {
+	if (num1 > num2 && num1 > num3)
+		return num1;
+	else if (num2 > num1 && num2 > num3)
+		return num2;
+	else
+		return num3;
+}
+
+int compares(int num1, int num2, int num3) {
+	if (num1 < num2 && num1 < num3)
+		return num1;
+	else if (num2 < num1 && num2 < num3)
+		return num2;
+	else
+		return num3;
 }
